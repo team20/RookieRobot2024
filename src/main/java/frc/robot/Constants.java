e0709808fe12cbe995b3405028d42b3f049981cf
Vincent-Christianson <jeffery.christianson@gmail.com>
2024-02-19 19:18:22 -0500

    idk

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -57,7 +57,7 @@ public final class Constants {
 
 	public static final class DriveConstants {
 		// CAN IDs (updated)
-		//public static final int kCounterWeightPort = 17;
+		public static final int kCounterWeightPort = 17;
 		public static final int kFrontRightDrivePort = 10;
 		public static final int kFrontRightSteerPort = 11;
 		public static final int kFrontLeftDrivePort = 40;
